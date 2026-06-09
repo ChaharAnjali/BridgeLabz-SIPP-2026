35909bda5e8dbe92bbb57032a70f97db9578e044
Chahar Anjali <anjalichahar.gla.ac.in@gmail.com>
2026-06-09 13:46:52 +0530

    Initial commit

---
+++ core-java-practice/gcr-codebase/programming-elements/RaviGuildMember.java
@@ -0,0 +1,17 @@
+
+public class RaviGuildMember {
+    public static void main(String[] args) {
+        String name="Ravi";
+        int age=22;
+        int rank=2;
+        long salary=50000;
+        float fee=5000;
+        int bonus=(int)(salary*12/100);
+        System.out.println("the name is:"+ name);
+        System.out.println("the age is:"+ age);
+        System.out.println("the rank is:"+ rank);
+        System.out.println("the salary is:"+ salary);
+        System.out.println("the membership fee is:"+ fee);
+        System.out.println("the bonus is:"+ bonus);
+    }
+}
